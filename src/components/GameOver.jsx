b2ab08bc7cdb54feff4b2b074898293013fb25af
Mike Thiem <mike.j.thiem@gmail.com>
2019-06-30 18:56:58 -0600

    Game over reason

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 export function GameOver(props) {
+  let loserReason;
+
+  switch (props.gameOverReason) {
+    case 'finishedWord':
+      loserReason = <p>{props.losingPlayer.name} lost by spelling the word: "{props.gameString}"</p>;
+      break;
+    case 'noPossibleWords':
+      loserReason = <p>{props.losingPlayer.name} lost because no word starts with "{props.gameString}"</p>;
+      break;
+    default:
+      loserReason = <p>Game ended for unknown reason</p>
+  }
+  
   return (
     <div className='App'>
       <h1>GAME OVER</h1>
-      <p>{props.losingPlayer.name} lost by spelling: "{props.gameString}"</p>
+      {loserReason}
       <input
         type='button'
         value='New Game'
