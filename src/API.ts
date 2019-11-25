aaf8ba2f3102337cc2a95b44c4a3229180b27868
Mike Thiem <mike.j.thiem@gmail.com>
2019-11-24 19:29:45 -0700

    Added try/catch blocks to all API calls so we at least get errors when a GET fails because the server's offline

--- src/API.ts
+++ src/API.ts
@@ -4,7 +4,6 @@ export class API
 {
     constructor()
     {
-
     }
 
     async checkForWord( testWord: string, minWordLength: number ): Promise<boolean>
@@ -15,43 +14,69 @@ export class API
             return false;
         }
         console.log( `Checking for word '${testWord}'...` );
-        const response = await window.fetch( `${serverUrl}/isword/${testWord}` );
-        const actualResponse = await response.json();
-        return actualResponse.isWord;
+        try
+        {
+            const response = await window.fetch( `${serverUrl}/isword/${testWord}` );
+            const actualResponse = await response.json();
+            return actualResponse.isWord;
+        }
+        catch ( error )
+        {
+            console.error( error );
+            return false;
+        }
     }
 
     async getPossibleWords( wordPart: string, saveToState: ( possibleWordList: string[] ) => void ): Promise<string[]>
     {
-        const response = await window.fetch( `${serverUrl}/possiblewords/${wordPart}` );
-        const possibleWordList = await response.json();
-        saveToState( possibleWordList );
-        return possibleWordList;
+        try
+        {
+            const response = await window.fetch( `${serverUrl}/possiblewords/${wordPart}` );
+            const possibleWordList = await response.json();
+            saveToState( possibleWordList );
+            return possibleWordList;
+        }
+        catch ( error )
+        {
+            console.error( error );
+            return [];
+        }
     }
 
     async getBlacklist(): Promise<string[]>
     {
-        const response = await window.fetch( `${serverUrl}/blacklist` );
-        return await response.json();
+        try
+        {
+            const response = await window.fetch( `${serverUrl}/blacklist` );
+            return await response.json();
+        }
+        catch ( error )
+        {
+            console.error( error );
+            return [];
+        }
     }
 
     async getWhitelist(): Promise<string[]>
     {
-        const response = await window.fetch( `${serverUrl}/whitelist` );
-        return await response.json();
+        try
+        {
+            const response = await window.fetch( `${serverUrl}/whitelist` );
+            return await response.json();
+        }
+        catch ( error )
+        {
+            console.error( error );
+            return [];
+        }
     }
 
     async blacklistWord( word: string ): Promise<void>
     {
-        const response = await window.fetch( `${serverUrl}/blacklist/${word}` );
-        console.log( response );
-    }
-
-    async whitelistWord( word: string ): Promise<void>
-    {
         try
         {
-            const data = await this.postData( `${serverUrl}/whitelist/${word}`, { word: word } );
-            console.log( JSON.stringify( data ) ); // JSON-string from `response.json()` call
+            const response = await window.fetch( `${serverUrl}/blacklist/${word}` );
+            console.log( response );
         }
         catch ( error )
         {
@@ -59,22 +84,16 @@ export class API
         }
     }
 
-    private postData = async ( url = '', data = {} ) =>
+    async whitelistWord( word: string ): Promise<void>
     {
-        // Default options are marked with *
-        const response = await fetch( url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify( data ) // body data type must match "Content-Type" header
-        } );
-        return await response.json(); // parses JSON response into native JavaScript objects
+        try
+        {
+            const data = await window.fetch( `${serverUrl}/whitelist/${word}` );
+            console.log( JSON.stringify( data ) ); // JSON-string from `response.json()` call
+        }
+        catch ( error )
+        {
+            console.error( error );
+        }
     }
 }
