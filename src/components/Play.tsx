f949efb2cf19c9b700b11c9a0110ee9f331392c0
Mike Thiem <mike.j.thiem@gmail.com>
2021-04-15 08:24:40 -0600

    Converted Play component from class to function Since it has no state anyways, and if I need to add state later I'll just use react hooks

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -6,13 +6,6 @@ import * as _ from 'underscore';
 import { ENTER_KEY_CODE } from '../constants';
 import { NumberedList } from './NumberedList';
 
-const initialState: PlayState = {
-}
-
-export interface PlayState
-{
-}
-
 export interface PlayProps
 {
     handleNextCharChange( event: any ): void;
@@ -28,92 +21,33 @@ export interface PlayProps
     waitingForAiToChooseLetter: boolean;
 }
 
-export class Play extends React.Component<PlayProps, PlayState> {
-    constructor( props: PlayProps )
-    {
-        super( props );
-        this.state = initialState;
-    }
+export function Play(props: PlayProps) {
 
-    submitChar = () =>
+    const submitChar = () =>
     {
-        if ( this.props.nextChar.length === 0 )
+        if ( props.nextChar.length === 0 )
         {
             alert( "Can't submit a blank" );
             return;
         }
-        this.props.commitNextChar();
+        props.commitNextChar();
     }
     
-    onKeyDown = ( event: React.KeyboardEvent<HTMLInputElement> ) =>
+    const onKeyDown = ( event: React.KeyboardEvent<HTMLInputElement> ) =>
     {
         if ( event.keyCode === ENTER_KEY_CODE )
-            this.submitChar();
+            submitChar();
     }
 
-    render()
+    
+    const buildPossibleWordList = () =>
     {
-        const possibleWordListForDisplay = this.props.displayWordList && this.buildPossibleWordList();
-
-        const bullshitButton = this.props.gameString.length > 0 &&
-            <Button
-                onClick={ this.props.handleCallBullshit }
-                text={ `Call Bullshit on ${this.props.getPreviousPlayer().name}` }
-                className="wideButton"
-            />;
-
-        return (
-            <>
-                <h1>Ghost</h1>
-                <Button
-                    id={ 1 }
-                    onClick={ this.props.handleExitGame }
-                    text='Quit Game'
-                />
-                <p>It is currently { this.props.getCurrentPlayer().name }'s turn</p>
-
-                <Spinner
-                    loading={ this.props.waitingForAiToChooseLetter }
-                />
-
-                <p>Enter the next character:</p>
-                <input
-                    autoComplete='off'
-                    onChange={ this.props.handleNextCharChange }
-                    type='text'
-                    id='nextChar'
-                    maxLength={ 1 }
-                    onKeyDown={ this.onKeyDown }
-                    value={ this.props.nextChar }
-                    width={5}
-                />
-                <Button
-                id={ 2 }
-                text="Submit letter"
-                onClick={this.submitChar}
-                />
-                <p>Game String:</p>
-                <input
-                    autoComplete='off'
-                    value={ this.props.gameString }
-                    type='text'
-                    id='gameString'
-                    readOnly={ true }
-                />
-                { bullshitButton }
-                { possibleWordListForDisplay }
-            </>
-        );
-    }
-
-    private buildPossibleWordList()
-    {
-        const randomSubsetOfPossibleWords: string[] = _.sample( this.props.possibleWordList, 20 );
+        const randomSubsetOfPossibleWords: string[] = _.sample( props.possibleWordList, 20 );
 
         return (
             <>
                 <br />
-                Random sample of possible words starting with "{this.props.gameString}":
+                Random sample of possible words starting with "{props.gameString}":
                 {
                 randomSubsetOfPossibleWords.length > 0
                     ? <NumberedList data={ randomSubsetOfPossibleWords } />
@@ -122,4 +56,56 @@ export class Play extends React.Component<PlayProps, PlayState> {
             </>
         )
     }
+
+    const possibleWordListForDisplay = props.displayWordList && buildPossibleWordList();
+
+    const bullshitButton = props.gameString.length > 0 &&
+        <Button
+            onClick={ props.handleCallBullshit }
+            text={ `Call Bullshit on ${props.getPreviousPlayer().name}` }
+            className="wideButton"
+        />;
+
+    return (
+        <>
+            <h1>Ghost</h1>
+            <Button
+                id={ 1 }
+                onClick={ props.handleExitGame }
+                text='Quit Game'
+            />
+            <p>It is currently { props.getCurrentPlayer().name }'s turn</p>
+
+            <Spinner
+                loading={ props.waitingForAiToChooseLetter }
+            />
+
+            <p>Enter the next character:</p>
+            <input
+                autoComplete='off'
+                onChange={ props.handleNextCharChange }
+                type='text'
+                id='nextChar'
+                maxLength={ 1 }
+                onKeyDown={ onKeyDown }
+                value={ props.nextChar }
+                width={5}
+            />
+            <Button
+            id={ 2 }
+            text="Submit letter"
+            onClick={submitChar}
+            />
+            <p>Game String:</p>
+            <input
+                autoComplete='off'
+                value={ props.gameString }
+                type='text'
+                id='gameString'
+                readOnly={ true }
+            />
+            { bullshitButton }
+            { possibleWordListForDisplay }
+        </>
+    );
 }
